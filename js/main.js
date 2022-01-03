67f751444bde975f053f41be802113c409bcbf64
Khizri Tataev <bodycat@gmail.com>
2022-01-03 19:42:33 +0300

    Eshop with JS and VUE
    HW06 in process, doing 6 7 8

--- js/main.js
+++ js/main.js
@@ -4,4 +4,14 @@ $(function(){
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
     });
+
+    $('.tab').on('click', function (e){
+        e.preventDefault();
+        $('.tab').removeClass('tab--active');
+        $('.tabs-content').removeClass('tabs-content--active');
+
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+// $('#tab-1')
+    });
 });
